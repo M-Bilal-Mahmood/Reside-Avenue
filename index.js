879deb8b4121365d2718cd6bd88223c9d7ed3748
M-Bilal-Mahmood <itsbilalpersonal@gmail.com>
2026-06-09 12:25:38 +0500

    new footer is updated

--- index.js
+++ index.js
@@ -25,14 +25,15 @@ const port = process.env.PORT || 3000;
 // ================================
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.engine("ejs", ejsMate);
-
 app.use(methodOverride("_method"));
 
+// ================================
+// Session Middleware
+// ================================
 const mongoSession = MongoStore.create({
     mongoUrl: process.env.ATLAS_DB,
     crypto: {
